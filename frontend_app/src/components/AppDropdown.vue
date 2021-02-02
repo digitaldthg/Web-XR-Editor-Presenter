5b13dfc82f08e95c6a460a48b59317b5456eca68
Lea Lazer <lea.schorling@gmail.com>
2021-02-02 13:23:53 +0100

    AppDropdown

---
+++ frontend_app/src/components/AppDropdown.vue
@@ -0,0 +1,59 @@
+<template>
+  <vue-dropdown :config="config" @setSelectedOption="dropdownChanged($event)"></vue-dropdown>
+</template>
+
+<script>
+import VueDropdown from "vue-dynamic-dropdown";
+export default {
+  name: "AppDropdown",
+  props: {
+	  placeholder: {
+			type: String,
+			default: 'Placeholder'
+		},
+	  color: {
+			type: String,
+			default: 'blue'
+		}
+  },
+  components: {
+    VueDropdown,
+  },
+  computed: {
+    config() {
+      return {
+          options: [
+            {
+              value: "option 1",
+            },
+            {
+              value: "option 2",
+            },
+          ],
+          prefix: this.placeholder,
+          backgroundColor: this.color,
+        }
+    },
+  },
+  methods: {
+	  dropdownChanged(){
+		  
+	  }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables.scss";
+@import "../styles/main.scss";
+</style>
+
+<docs>
+### Examples
+
+```jsx
+<AppDropdown color="red" placeholder="Custom Placeholder"></AppDropdown >
+```
+
+
+</docs>
