275416033cd58f89655875e007ebe1764f03cd46
invisibleSascha <sascha@invisibleroom.com>
2021-03-01 14:56:32 +0100

    IP und cors settings

--- frontend_app/vue.config.js
+++ frontend_app/vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   devServer: {
-    https: true
+    https: true,
+    headers: { 'Access-Control-Allow-Origin': '*' }
   },
   chainWebpack: config => {
     config.module.rules.delete("svg");
