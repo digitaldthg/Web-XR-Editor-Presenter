2a7b1b106f18cadc910af466d2603fda8bebf7f6
Lea Lazer <lea.schorling@gmail.com>
2021-03-01 15:03:12 +0100

    slide preview

--- frontend_app/src/partials/SlidePreview.vue
+++ frontend_app/src/partials/SlidePreview.vue
@@ -1,42 +1,62 @@
 <template>
-  <div :class="'preview visible-'+ CheckVisibility()">
-{{slide.Name}}
-<img v-if="slide.PreviewImage != null" :src="GetImageUrl(slide.PreviewImage.url)"/>
+  <div :class="'preview visible-' + CheckVisibility()">
+    
+    <img class="preview-image"
+      v-if="slide.PreviewImage != null"
+      :src="GetImageUrl(slide.PreviewImage.url)"
+    />
+    <div class="title-text">{{ slide.Name }}</div>
   </div>
 </template>
 
 <script>
 export default {
   name: "SlidePreview",
-  props:{
-    slide: null
+  props: {
+    slide: null,
   },
-  methods:{
-    GetImageUrl(path){
-    return this.$store.state.cmsAPIUrl + path;
+  methods: {
+    GetImageUrl(path) {
+      return this.$store.state.cmsAPIUrl + path;
     },
-    CheckVisibility(){
-      return this.slide.id == this.$store.state.currentSelectedSlideContainer.Slides[this.$store.state.currentSlideIdx].id
-    }
-  }
+    CheckVisibility() {
+      return (
+        this.slide.id ==
+        this.$store.state.currentSelectedSlideContainer.Slides[
+          this.$store.state.currentSlideIdx
+        ].id
+      );
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../styles/variables.scss";
-.preview{
+.preview {
   height: 100%;
   width: $slideshow-height;
   background: white;
-  border-style: solid
+  border-style: solid;
+  overflow: hidden;
+  position: relative;
 }
 
-.visible-true{
+
+.visible-true {
   border-width: 5px;
 }
 
-.visible-false{
+.visible-false {
   border-width: 1px;
 }
 
+.title-text{
+  position: absolute;
+  top:5px;
+  width: 100px;
+}
+.preview-image{
+
+}
 </style>
